5fb4839492dc2a4d1cc30395e330b61b29edaa2e
Aman Sethi <35420536+amansethi00@users.noreply.github.com>
2021-01-14 23:43:43 +0530

    feat:add text input

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import bgLight from "./images/bg-desktop-light.jpg";
+import bgDark from "./images/bg-desktop-dark.jpg";
+import bgLightMobile from "./images/bg-mobile-light.jpg";
+import bgLightDark from "./images/bg-mobile-dark.jpg";
 
+const OPTIONS = ["One", "Two", "Three"];
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div className="header-background">
+      <img src={bgLight} alt="background-image" className="bg-header"/>
+      </div>
+      <div className="container">
+      
+          <h1 className="head">TODO</h1>
+           
+        <div><input type="text" className="list" placeholder="Create a new todo"></input></div>
+        <div className="list-container">
+
+
+        </div>
+         items left
+
+          All
+          Active 
+          Completed
+
+          Clear Completed
+
+          Drag and drop to reorder list
+     
+            </div>
+            </div>
+      );
 }
 
 export default App;
