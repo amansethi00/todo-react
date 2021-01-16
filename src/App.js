6b045179d6b540db6969bc89a1d346e09041bf0b
Aman Sethi <35420536+amansethi00@users.noreply.github.com>
2021-01-16 15:37:35 +0530

    feat:completed working

--- src/App.js
+++ src/App.js
@@ -9,21 +9,94 @@ import bgLightDark from "./images/bg-mobile-dark.jpg";
 
 function App() {
   const [newItem,setNewItem]=useState("");
-  const [OPTIONS,setOptions] = useState(["One", "Two", "Three"]);
+  const [completed,setCompleted] = useState("");
+  const [all,setAll] = useState("");
+  const [OPTIONS,setOptions] = useState([]);
+  
+  const [remaining,setRemaining]=useState(OPTIONS.length);
   function fetchValue(event){
     setNewItem(event.target.value);
   } 
   function addToList(event){
         if(event.key ==="Enter" && newItem!==""){
          
-          let newArr = [...OPTIONS,newItem];
+          let newArr = [...OPTIONS,{value:newItem,completed:false}];
           setOptions(newArr);
+          setRemaining(remaining+1);
           console.log(newArr);
+          event.target.value="";
         }
         
       
   }
- 
+  function checkHandler(event){
+    // if(OPTIONS[OPTIONS.filter(s=>s.name).findIndex(event.target.value)].completed ===false){
+    //   return false;
+    // }
+    // else{
+    //   return true;
+    // }
+    if(OPTIONS===completed){
+      return true;
+    }
+    else{
+      // if(event.target.value.completed === false){
+      //   return false;
+      // }
+      // else{
+      //   return true;
+      // }
+      return "";
+
+    }
+   
+  }
+ function checkBoxHandler(event){
+    // event.target.checked =!event.target.checked;
+    if(OPTIONS===completed){
+
+      // event.target.checked =true;
+      console.log(event);
+      console.log("completd m ho aap janab");
+    }
+    else{
+      console.log(event.target.checked);
+      if(event.target.checked ===true){
+        console.log(event.target.value);
+        OPTIONS.forEach(s=> {
+          if(s.value === event.target.value){
+            s.completed=true;
+          }
+        });
+        
+      }
+      else{
+        OPTIONS.forEach(s=> {
+          if(s.value === event.target.value){
+            s.completed=false;
+          }
+        });
+      }
+      console.log(OPTIONS);
+
+    }
+   
+    
+    
+    
+ }
+ function completedHandler(){
+  setAll(OPTIONS);
+   let completed = OPTIONS.filter(s=>s.completed===true);
+   setCompleted(completed);
+   
+   setOptions(completed);
+ }
+ function allHandler(){
+   setOptions(all);
+   setCompleted(completed);
+
+ }
   
   return (
    
@@ -36,26 +109,34 @@ function App() {
       
           <h1 className="head">TODO</h1>
            
-        <div><input type="text" className="list" placeholder="Create a new todo" onKeyPress={addToList} onChange={fetchValue}></input></div>
+        <div><input type="text" className="list" placeholder="Create a new todo" onKeyPress={addToList} onChange={fetchValue} ></input></div>
         <div className="useroutput" >
+        
         {OPTIONS.map((value, index) => {
           return (
             <div key={index} id="output" className="output-list">
-              <input type="checkbox"></input> {value}
+              <input type="checkbox" className="checkbox" 
+              onClick={checkBoxHandler} value={value.value} defaultChecked={value.completed}></input> {value.value}
               <br/> 
             </div>
 
           );
         })}
+        <div className="span-bar">
+          <div id="items=left" className="span-items">{remaining} items left</div>
+          <div className="span-items" id="all" onClick={allHandler}> All</div>
+          <div className="span-items" id="active"> Active</div>
+          <div className="span-items" id="completed" onClick={completedHandler} > Completed</div>
+          <div id="clear" className="span-items">Clear Completed
+</div></div>
+        
       </div>
-         items left
+         
 
-          All
-          Active 
-          Completed
+         
 
-          Clear Completed
 
+          
           Drag and drop to reorder list
      
             </div>
